7966b416adb6971dea6ff458a2cb259937cf87ae
Jesus Rosales Castillo <jesusrosales07537@gmail.com>
2025-08-04 15:57:42 -0600

    feat: configuracion inicial de electron

---
+++ main.js
@@ -0,0 +1,36 @@
+const { BrowserWindow, app } = require("electron");
+
+function createWindow() {
+    const windows = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    })
+
+    windows.loadFile("index.html");
+    
+}
+
+
+app.whenReady().then( () => {
+    // crear la ventana principal
+    createWindow();
+
+    // abir una nueva ventana cuando se haga clic en el icono de la aplicacion en el dock (macOS)
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    })
+})
+
+
+// cerrar la aplicacion cuando todas las ventanas esten cerradas en sistemas que no sean macOS
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
