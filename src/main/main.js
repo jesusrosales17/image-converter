0cb4a056e8fb33c8ea454e2487a807ab338f6e53
Jesus Rosales Castillo <jesusrosales07537@gmail.com>
2025-08-04 16:09:35 -0600

    fix: quitado carpeta node modules

--- src/main/main.js
+++ src/main/main.js
@@ -6,7 +6,7 @@ function createWindow() {
         height: 600,
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false,
+            contextIsolation: true,
         },
     })
 
